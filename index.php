138c33cf189eb40050511753ade4891238156887
AZSharper <84278174+AZSharper@users.noreply.github.com>
2021-05-16 19:38:33 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+<title> WEBSITE </title>
+<body>
+<h1>
+<center>
+<IMG SRC="azs-logo.png" ALT="AZSharper Logo"><br>
+<?php
+  echo "Internal IP:-";
+echo $_SERVER['SERVER_ADDR'];
+  echo "          | Your IP:-";
+  // PHP code to extract IP  
+  
+function getVisIpAddr() { 
+      
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) { 
+        return $_SERVER['HTTP_CLIENT_IP']; 
+    } 
+    else if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) { 
+        return $_SERVER['HTTP_X_FORWARDED_FOR']; 
+    } 
+    else { 
+        return $_SERVER['REMOTE_ADDR']; 
+    } 
+} 
+  
+// Store the IP address 
+$vis_ip = getVisIPAddr(); 
+  
+// Display the IP address 
+echo $vis_ip; 
+?>
+</center>
+</h1>
+</body>
+</html>
